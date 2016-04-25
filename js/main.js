4005f5ad547d69a947f19fa7355f69358410e5db
Jeremy Sykes <jsykes@tvo.org>
2016-04-25 14:17:06 -0400

    Added array of css colors and modified functionality to recognize those colors

--- js/main.js
+++ js/main.js
@@ -1,3 +1,153 @@
+var cssColorNames = [
+    'AliceBlue',
+    'AntiqueWhite',
+    'Aqua',
+    'Aquamarine',
+    'Azure',
+    'Beige',
+    'Bisque',
+    'Black',
+    'BlanchedAlmond',
+    'Blue',
+    'BlueViolet',
+    'Brown',
+    'BurlyWood',
+    'CadetBlue',
+    'Chartreuse',
+    'Chocolate',
+    'Coral',
+    'CornflowerBlue',
+    'Cornsilk',
+    'Crimson',
+    'Cyan',
+    'DarkBlue',
+    'DarkCyan',
+    'DarkGoldenRod',
+    'DarkGray',
+    'DarkGrey',
+    'DarkGreen',
+    'DarkKhaki',
+    'DarkMagenta',
+    'DarkOliveGreen',
+    'Darkorange',
+    'DarkOrchid',
+    'DarkRed',
+    'DarkSalmon',
+    'DarkSeaGreen',
+    'DarkSlateBlue',
+    'DarkSlateGray',
+    'DarkSlateGrey',
+    'DarkTurquoise',
+    'DarkViolet',
+    'DeepPink',
+    'DeepSkyBlue',
+    'DimGray',
+    'DimGrey',
+    'DodgerBlue',
+    'FireBrick',
+    'FloralWhite',
+    'ForestGreen',
+    'Fuchsia',
+    'Gainsboro',
+    'GhostWhite',
+    'Gold',
+    'GoldenRod',
+    'Gray',
+    'Grey',
+    'Green',
+    'GreenYellow',
+    'HoneyDew',
+    'HotPink',
+    'IndianRed',
+    'Indigo',
+    'Ivory',
+    'Khaki',
+    'Lavender',
+    'LavenderBlush',
+    'LawnGreen',
+    'LemonChiffon',
+    'LightBlue',
+    'LightCoral',
+    'LightCyan',
+    'LightGoldenRodYellow',
+    'LightGray',
+    'LightGrey',
+    'LightGreen',
+    'LightPink',
+    'LightSalmon',
+    'LightSeaGreen',
+    'LightSkyBlue',
+    'LightSlateGray',
+    'LightSlateGrey',
+    'LightSteelBlue',
+    'LightYellow',
+    'Lime',
+    'LimeGreen',
+    'Linen',
+    'Magenta',
+    'Maroon',
+    'MediumAquaMarine',
+    'MediumBlue',
+    'MediumOrchid',
+    'MediumPurple',
+    'MediumSeaGreen',
+    'MediumSlateBlue',
+    'MediumSpringGreen',
+    'MediumTurquoise',
+    'MediumVioletRed',
+    'MidnightBlue',
+    'MintCream',
+    'MistyRose',
+    'Moccasin',
+    'NavajoWhite',
+    'Navy',
+    'OldLace',
+    'Olive',
+    'OliveDrab',
+    'Orange',
+    'OrangeRed',
+    'Orchid',
+    'PaleGoldenRod',
+    'PaleGreen',
+    'PaleTurquoise',
+    'PaleVioletRed',
+    'PapayaWhip',
+    'PeachPuff',
+    'Peru',
+    'Pink',
+    'Plum',
+    'PowderBlue',
+    'Purple',
+    'Red',
+    'RosyBrown',
+    'RoyalBlue',
+    'SaddleBrown',
+    'Salmon',
+    'SandyBrown',
+    'SeaGreen',
+    'SeaShell',
+    'Sienna',
+    'Silver',
+    'SkyBlue',
+    'SlateBlue',
+    'SlateGray',
+    'SlateGrey',
+    'Snow',
+    'SpringGreen',
+    'SteelBlue',
+    'Tan',
+    'Teal',
+    'Thistle',
+    'Tomato',
+    'Turquoise',
+    'Violet',
+    'Wheat',
+    'White',
+    'WhiteSmoke',
+    'Yellow',
+    'YellowGreen'
+];
+
 var myRec = new p5.SpeechRec();
 
 function setup() {
@@ -10,7 +160,7 @@ function setup() {
     // Instructions
     textSize(32);
     textAlign(CENTER);
-    text('Say the name of a colour', width / 2, height / 2);
+    text('Say the name of a color.', width / 2, height / 2);
     myRec.continuous = true; // Do continuous recognition
 
     // myRec.interimResults = true; // Allow partial recognition (faster, less accurate)
@@ -21,25 +171,31 @@ function setup() {
 function draw() {}
 
 function showColor(colorName) {
-	var colors = {
-		'red': '#FF0000',
-		'yellow': '#FFFF00',
-		'blue': '#0000FF',
-		'orange': '#FFA500',
-		'purple': '#800080',
-		'green': '#00FF00'
-	}
-	if (colors[colorName]) {
-		background(color(colors[colorName]));
+	if (cssColorNames.indexOf(colorName) > -1) {
+        background(color(colorName));
 	} else {
 		background(color('#000000'));
 	}
 }
 
-function showResult() {
-    if (myRec.resultValue === true) {
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
 
-    	showColor(myRec.resultString.toLowerCase());
+function removeSpacesFromString(str){
+    return str.replace(/\s/g, '');
+}
+
+function showResult() {
+    console.log('showResult');
+    if (myRec.resultValue === true) {
+        var lowerCaseString = myRec.resultString.toLowerCase();
+        var titleCaseString = toTitleCase(lowerCaseString);
+        var colorString = removeSpacesFromString(titleCaseString);
+
+    	showColor(colorString);
         text(myRec.resultString, width / 2, height / 2);
     }
 }
